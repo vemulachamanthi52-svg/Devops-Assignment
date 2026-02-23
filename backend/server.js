88fb0053dc1d11b755348d9323036b2e4aa5f275
chamanthi <vivek@gmail.com>
2026-02-24 02:52:14 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
